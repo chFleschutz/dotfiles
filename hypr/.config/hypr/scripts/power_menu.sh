640d0414c07c88c0f412bfe1191f3e9520e9b6bd
Christoph Fleschutz <ch.fleschutz@gmail.com>
2026-01-04 18:00:27 +0100

    Add power button

---
+++ hypr/.config/hypr/scripts/power_menu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define the options
+options="   Power Off\n 󰜉  Reboot\n 󰤄  Suspend\n 󰗼  Log Out\n   Lock"
+
+# Launch Rofi and capture the selection
+chosen=$(echo -e "$options" | rofi -dmenu -i -p "System Action:" -theme-str 'window {width: 15%;}')
+
+case $chosen in
+    "   Power Off") poweroff ;;
+    " 󰜉  Reboot") reboot ;;
+    " 󰤄  Suspend") systemctl suspend ;;
+    " 󰗼  Log Out") hyprctl dispatch exit ;;
+    "   Lock") hyprlock ;; # Or your preferred locker
+esac
