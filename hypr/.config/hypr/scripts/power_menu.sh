72d08d4afb3ff8509fc6ae57e5069e8d7952c3f7
Christoph Fleschutz <ch.fleschutz@gmail.com>
2026-01-06 19:58:39 +0100

    Fix typo in power menu

--- hypr/.config/hypr/scripts/power_menu.sh
+++ hypr/.config/hypr/scripts/power_menu.sh
@@ -4,7 +4,7 @@
 options="   Power Off\n 󰜉  Reboot\n 󰤄  Suspend\n 󰗼  Log Out\n   Lock"
 
 # Launch Rofi and capture the selection
-chosen=$(echo -e "$options" | rofi -dmenu -i -p "System Action:" -theme-str 'window {width: 15%;}')
+chosen=$(echo -e "$options" | rofi -dmenu -i -p "System Action" -theme-str 'window {width: 15%;}')
 
 case $chosen in
     "   Power Off") poweroff ;;
